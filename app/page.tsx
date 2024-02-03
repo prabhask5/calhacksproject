ee93991e6892b4b62af6505e336e2cc07bea5092
Prabhas Kurapati <prabhask@berkeley.edu>
2024-02-02 22:17:44 -0800

    add validation

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import Script from "next/script";
 import { useEffect, useState } from "react";
 import 'flowbite';
 import { Toast } from "flowbite-react";
-import { HiCheck } from "react-icons/hi";
+import { HiCheck, HiX } from "react-icons/hi";
 
 export default function Home() {
 
@@ -18,40 +18,140 @@ export default function Home() {
   const [essay, setEssay] = useState("");
   const [is18, setIs18] = useState(false);
   const [showToast, setShowToast] = useState(false);
+  const [toastState, setToastState] = useState<"success" | "failure">("success");
+
+  const [fullNameFieldError, setFullNameFieldError] = useState(false);
+  const [universityFieldError, setUniversityFieldError] = useState(false);
+  const [majorFieldError, setMajorFieldError] = useState(false);
+  const [gradDateFieldError, setGradDateFieldError] = useState(false);
+  const [genderFieldError, setGenderFieldError] = useState(false);
+  const [countryFieldError, setCountryFieldError] = useState(false);
+  const [resumeFieldError, setResumeFieldError] = useState(false);
+  const [essayFieldError, setEssayFieldError] = useState(false);
+
+  const validateFullName = () => {
+    const pattern = /^[a-z ,.'-]+$/i;
+    const valid = name.replace(/\s/g, '') !== "" && pattern.test(name);
+    setFullNameFieldError(!valid);
+    return valid;
+  };
+
+  const validateUniversity = () => {
+    const valid = university.replace(/\s/g, '') !== "";
+    setUniversityFieldError(!valid);
+    return valid;
+  };
+
+  const validateMajor = () => {
+    const valid = major.replace(/\s/g, '') !== "";
+    setMajorFieldError(!valid);
+    return valid;
+  };
+
+  const validateGradDate = () => {
+    const pattern = /^[0-9]*$/;
+    const valid = gradDate.replace(/\s/g, '') !== "" && pattern.test(gradDate);
+    setGradDateFieldError(!valid);
+    return valid;
+  };
+
+  const validateGender = () => {
+    const valid = ["male", "female", "nonbinary", "other"].includes(gender);
+    setGenderFieldError(!valid);
+    return valid;
+  };
+
+  const validateCountry = () => {
+    const valid = country.replace(/\s/g, '') !== "";
+    setCountryFieldError(!valid);
+    return valid;
+  };
+
+  const validateResume = () => {
+    const valid = resume != null;
+    setResumeFieldError(!valid);
+    return valid;
+  };
+
+  const validateEssay = () => {
+    const valid = essay.replace(/\s/g, '') !== "";
+    setEssayFieldError(!valid);
+    return valid;
+  };
+
+  const validateAll = () => {
+    const vfn = validateFullName();
+    const vu = validateUniversity();
+    const vm = validateMajor();
+    const vgd = validateGradDate();
+    const vg = validateGender();
+    const vc = validateCountry();
+    const vr = validateResume();
+    const ve = validateEssay();
+
+    return (
+        vfn &&
+        vu &&
+        vm &&
+        vgd &&
+        vg &&
+        vc &&
+        vr &&
+        ve
+    );
+  };
 
   const toastClassState = !showToast ? " opacity-0 ease-in-out cursor-default": " opacity-1 ease-in-out";
   const toastDismissClassState = !showToast ? "cursor-default": "cursor-pointer";
+  const toastIconClassState = toastState == "success" ? " bg-green-100 text-green-500": " bg-red-100 text-red-500";
+  const textInputBaseState = "border text-sm rounded-lg block w-full p-2.5";
+  const validInputAddOnState = " focus:ring-blue-500 focus:border-blue-500 border-gray-300 text-gray-900 bg-gray-50";
+  const invalidInputAddOnState = " bg-red-50 border-red-500 text-red-900 placeholder-red-700 focus:ring-red-500 focus:border-red-500";
+  const fileInputBaseState = "block w-full text-sm border rounded-lg cursor-pointer focus:outline-none";
+  const validFileInputAddOnState = " text-gray-900 bg-gray-50";
+  const invalidFileInputAddOnState = " bg-red-50 text-red-900";
+  const labelBaseState = "block mb-2 text-sm font-medium";
+  const validLabelAddOnState = " text-gray-900";
+  const invalidLabelAddOnState = " text-red-700";
 
   useEffect(() => {
     if (showToast) setTimeout(() => setShowToast(false), 3000);
   }, [showToast])
 
   const handleSubmit = () => {
-    const formData = {
-      name,
-      university,
-      major,
-      gradDate,
-      gender,
-      country,
-      resume,
-      essay,
-      is18,
+    const allValid = validateAll();
+
+    if (allValid) {
+      const formData = {
+        name,
+        university,
+        major,
+        gradDate,
+        gender,
+        country,
+        resume,
+        essay,
+        is18,
+      }
+      console.log(formData);
+
+      setToastState("success");
+      setShowToast(true);
+  
+      setName("");
+      setUniversity("");
+      setMajor("");
+      setGradDate("");
+      setGender("");
+      setCountry("");
+      setResume(null);
+      (document.getElementById("resume") as HTMLInputElement).value = "";
+      setEssay("");
+      setIs18(false);
+    } else {
+      setToastState("failure");
+      setShowToast(true);
     }
-    console.log(formData);
-
-    setShowToast(true);
-
-    setName("");
-    setUniversity("");
-    setMajor("");
-    setGradDate("");
-    setGender("");
-    setCountry("");
-    setResume(null);
-    (document.getElementById("resume") as HTMLInputElement).value = "";
-    setEssay("");
-    setIs18(false);
   };
 
   return (
@@ -61,24 +161,24 @@ export default function Home() {
       handleSubmit();
     }} className="max-w-sm mx-auto my-6">
       <div className="mb-5">
-        <label htmlFor="fullname" className="block mb-2 text-sm font-medium text-gray-900">Full name</label>
-        <input type="text" id="fullname" value={name} onChange={e => setName(e.target.value)} className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" />
+        <label htmlFor="fullname" className={labelBaseState + (fullNameFieldError ? invalidLabelAddOnState: validLabelAddOnState)}>Full name</label>
+        <input type="text" id="fullname" value={name} onChange={e => setName(e.target.value)} onBlur={() => validateAll()} className={textInputBaseState + (fullNameFieldError ? invalidInputAddOnState: validInputAddOnState)} />
       </div>
       <div className="mb-5">
-        <label htmlFor="university" className="block mb-2 text-sm font-medium text-gray-900">University</label>
-        <input type="text" id="university" value={university} onChange={e => setUniversity(e.target.value)} className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"/>
+        <label htmlFor="university" className={labelBaseState + (universityFieldError ? invalidLabelAddOnState: validLabelAddOnState)}>University</label>
+        <input type="text" id="university" value={university} onChange={e => setUniversity(e.target.value)} onBlur={() => validateAll()} className={textInputBaseState + (universityFieldError ? invalidInputAddOnState: validInputAddOnState)}/>
       </div>
       <div className="mb-5">
-        <label htmlFor="major" className="block mb-2 text-sm font-medium text-gray-900">Major</label>
-        <input type="text" id="major" value={major} onChange={e => setMajor(e.target.value)} className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"/>
+        <label htmlFor="major" className={labelBaseState + (majorFieldError ? invalidLabelAddOnState: validLabelAddOnState)}>Major</label>
+        <input type="text" id="major" value={major} onChange={e => setMajor(e.target.value)} onBlur={() => validateAll()} className={textInputBaseState + (majorFieldError ? invalidInputAddOnState: validInputAddOnState)}/>
       </div>
       <div className="mb-5">
-        <label htmlFor="gradDate" className="block mb-2 text-sm font-medium text-gray-900">Graduation date</label>
-        <input type="text" id="gradDate" value={gradDate} onChange={e => setGradDate(e.target.value)} className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"/>
+        <label htmlFor="gradDate" className={labelBaseState + (gradDateFieldError ? invalidLabelAddOnState: validLabelAddOnState)}>Graduation date</label>
+        <input type="text" id="gradDate" value={gradDate} onChange={e => setGradDate(e.target.value)} onBlur={() => validateAll()} className={textInputBaseState + (gradDateFieldError ? invalidInputAddOnState: validInputAddOnState)}/>
       </div>
       <div className="mb-5">
-        <label htmlFor="gender" className="block mb-2 text-sm font-medium text-gray-900">Gender</label>
-        <select id="gender" value={gender} onChange={e => setGender(e.target.value)} className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5">
+        <label htmlFor="gender" className={labelBaseState + (genderFieldError ? invalidLabelAddOnState: validLabelAddOnState)}>Gender</label>
+        <select id="gender" value={gender} onChange={e => setGender(e.target.value)} onBlur={() => validateAll()} className={textInputBaseState + (genderFieldError ? invalidInputAddOnState: validInputAddOnState)}>
           <option value="" disabled>Select gender</option>
           <option value="male">Male</option>
           <option value="female">Female</option>
@@ -87,18 +187,18 @@ export default function Home() {
         </select>
       </div>
       <div className="mb-5">
-        <label htmlFor="country" className="block mb-2 text-sm font-medium text-gray-900">Country of residence</label>
-        <input type="text" id="country" value={country} onChange={e => setCountry(e.target.value)} className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"/>
+        <label htmlFor="country" className={labelBaseState + (countryFieldError ? invalidLabelAddOnState: validLabelAddOnState)}>Country of residence</label>
+        <input type="text" id="country" value={country} onChange={e => setCountry(e.target.value)} onBlur={() => validateAll()} className={textInputBaseState + (countryFieldError ? invalidInputAddOnState: validInputAddOnState)}/>
       </div>
       <div className="mb-5">
-        <label className="block mb-2 text-sm font-medium text-gray-900" htmlFor="resume">Upload resume</label>
-        <input accept="application/pdf" onChange={e => setResume(e.target.files ? e.target.files[0]: null)} className="block w-full text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 focus:outline-none" id="resume" type="file"/>
+        <label className={labelBaseState + (resumeFieldError ? invalidLabelAddOnState: validLabelAddOnState)} htmlFor="resume">Upload resume</label>
+        <input accept="application/pdf" onChange={e => setResume(e.target.files ? e.target.files[0]: null)} onBlur={() => validateAll()} className={fileInputBaseState + (resumeFieldError ? invalidFileInputAddOnState: validFileInputAddOnState)} id="resume" type="file"/>
         {resume && <div className="mt-1 cursor-pointer text-sm hover:underline text-gray-500" onClick={() => window.open(URL.createObjectURL(resume), "_blank")}>Click to preview file</div>}
       </div>
       <div className="mb-5">
-        <label htmlFor="message" className="block mb-2 text-sm font-medium text-gray-900">Why are you interested in attending Cal Hacks 11.0? What are you interested in
+        <label htmlFor="essay" className={labelBaseState + (essayFieldError ? invalidLabelAddOnState: validLabelAddOnState)}>Why are you interested in attending Cal Hacks 11.0? What are you interested in
 building? (1000 character maximum):</label>
-        <textarea value={essay} onChange={e => setEssay(e.target.value)}  id="message" rows={4} className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500"></textarea>
+        <textarea value={essay} onChange={e => setEssay(e.target.value)} onBlur={() => validateAll()} id="essay" rows={4} className={textInputBaseState + (essayFieldError ? invalidInputAddOnState: validInputAddOnState)}></textarea>
       </div>
       <div className="flex items-start mb-5">
         <div className="flex items-center h-5">
@@ -109,10 +209,10 @@ building? (1000 character maximum):</label>
       <button type="submit" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center">Submit</button>
     </form>
     <Toast className={"fixed bottom-5 right-5" + toastClassState}>
-      <div className="inline-flex h-8 w-8 shrink-0 items-center justify-center rounded-lg bg-green-100 text-green-500 dark:bg-green-800 dark:text-green-200">
-        <HiCheck className="h-5 w-5" />
+      <div className={"inline-flex h-8 w-8 shrink-0 items-center justify-center rounded-lg" + toastIconClassState}>
+        {toastState == "success" ? <HiCheck className="h-5 w-5" />: <HiX className="h-5 w-5" />}
       </div>
-      <div className="ml-3 text-sm font-normal">Form successfully submitted.</div>
+      <div className="ml-3 text-sm font-normal">{toastState == "success" ? "Form successfully submitted.": "Please fix invalid responses."}</div>
       <Toast.Toggle className={toastDismissClassState} onDismiss={() => setShowToast(false)}/>
     </Toast>
     <Script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.2.1/flowbite.min.js"></Script>
